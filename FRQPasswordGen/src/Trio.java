b2f730333aa2693201059576096550c11788a3e7
Phil Marcus <pmarcus@reverek12.org>
2024-01-26 09:00:53 -0500

    completed Trio

--- FRQPasswordGen/src/Trio.java
+++ FRQPasswordGen/src/Trio.java
@@ -1,4 +1,30 @@
 
 public class Trio {
 
+	private Salad salad;
+	private Sandwich sandwich;
+	private Drink drink;
+	
+	public Trio(Sandwich sw, Salad sd, Drink d) {
+		sandwich= sw;
+		salad = sd;
+		drink = d;
+	}
+	
+	public String getName() {
+		return (sandwich.getName() + "/"+salad.getName()+"/"+drink.getName()+" Trio");
+	}
+	
+	public double getPrice() {
+		double min = sandwich.getPrice();
+		if (salad.getPrice() < min) {
+			min = salad.getPRice();
+		}
+		if (drink.getPrice() < min) {
+			min = drink.getPrice();
+		}
+		
+		return sandwich.getPrice()+salad.getPrice()+drink.getPrice()-min;
+	}
+	
 }
